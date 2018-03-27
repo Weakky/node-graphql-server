2e3c5f0a6d9ea1d88112ddfaee39fccaed781fe9
Flavian DESVERNE <flavian.desverne@epitech.eu>
2018-03-27 18:43:16 +0200

    change to test

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,5 +1,5 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { Prisma } = require('prisma-binding')
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
